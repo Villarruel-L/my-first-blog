9b9daca694557b48326fbf42ee5977ad195e076c
Jesus <villarruel.luis@uabc.edu.mx>
2020-05-29 22:08:59 -0700

    quinto intento

--- blog/forms.py
+++ blog/forms.py
@@ -13,7 +13,7 @@ class PostForm(forms.ModelForm):
 
     class Meta: 
         model = Post 
-        fields = ('title', 'text',) 
+        fields = ('title', 'text') 
 
 class CommentForm(forms.ModelForm):
     class Meta:
